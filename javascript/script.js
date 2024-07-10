44302eb67860cca08ae267afe34464cdd615408b
romarioccalluche <ccallucher@gmail.com>
2024-07-09 20:13:04 -0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //inicio
+    $('#p1').click(function(){
+    $('#contenido').load("../inicio/inicio.html");});
+        
+    //nosotros
+    $('#p2').click(function(){
+    $('#contenido').load("../inicio/nosotros.html");});
+
+    //galeria
+    $('#p3').click(function(){
+    $('#contenido').load("../inicio/galeria.html");});
+
+
+    //pie de pagina
+    $('#pie1').click(function(){
+    $('#contenido').load("../inicio/inicio.html");});
+        
+    $('#pie2').click(function(){
+    $('#contenido').load("../inicio/nosotros.html");});
+
+    $('#pie3').click(function(){
+    $('#contenido').load("../inicio/galeria.html");});
+});
+
---
+++ javascript/tarjetas.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    //aji de gallina
+    $('#t1').click(function(){
+    $('#contenido').load("../tarjetas/ajigallina.html");});
+            
+    //picarones
+    $('#t2').click(function(){
+    $('#contenido').load("../tarjetas/picarones.html");});
+    
+    //pollo al horno
+    $('#t3').click(function(){
+    $('#contenido').load("../tarjetas/polloalhorno.html");});
+
+    //pollo a la braza
+    $('#t4').click(function(){
+        $('#contenido').load("../tarjetas/pollobraza.html");});
+
+});
